f6626576849e8c602f38f8c449107cdab22b445d
Jay-1501 <laherijay9348@gmail.com>
2022-08-01 23:22:24 +0530

    added speechly and complete project

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import {Card, CardHeader, CardContent, Typography, Divider, Grid } from '@material-ui/core'
-import Form from './Form/Form'
-import useStyles from './styles'
-import List from './List/List'
+import React, { useState, useEffect, useContext } from 'react';
+import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+import { useSpeechContext } from '@speechly/react-client';
+import { ExpenseTrackerContext } from '../../context/context';
+import useStyles from './styles';
+import Form from './Form/Form';
+import List from './List/List';
+import InfoCard from '../InfoCard';
 
-const Main = () => {
-    const classes = useStyles();
+const ExpenseTracker = () => {
+  const classes = useStyles();
+  const { balance } = useContext(ExpenseTrackerContext);
+
   return (
-    <div>
-      <Card className={classes.root}>
-        <CardHeader title="Expense Tracker" subheader='powered by speechly'/>
-        <CardContent>
-            <Typography align='center' variant='h5'>
-                Total Balance $100 
-            </Typography>
-            <Typography variant='subtitle1' style={{ lineHeight:'1.5em' ,marginTop:'20px'}}>
-                {/* Infocard */}
-                Try saying add income $100 in category salary for monday ... 
-            </Typography>
-            <Divider />
-            <Form />
-        </CardContent>
-        <CardContent className={classes.cardContent}>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <List />
-                </Grid>
-            </Grid>
-        </CardContent>
-      </Card>
-    </div>
-  )
-}
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+      <CardContent>
+        <Typography align="center" variant="h5">Total Balance ${balance}</Typography>
+        <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
+          <InfoCard />
+        </Typography>
+        <Divider className={classes.divider} />
+        <Form />
+      </CardContent>
+      <CardContent className={classes.cartContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <List />
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default Main;
+export default ExpenseTracker;
